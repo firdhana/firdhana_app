f170434b0fa59a98083c2fab6c4c621cf3622073
Donny Firdhana <firdhana@gmail.com>
2013-02-27 19:39:39 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirdhanaApp::Application.config.secret_token = 'fb614781f6fdebb9800c7140935995aa12c8e9b3a3c9994945b6de8e8f201ccbd16d24f4857983419b837fa4499d03af791bc64aaa419de56c4568d4f0e9c44c'
